fb80f48123517bad8f419a88118c9303c6779ff5
mohan <mohanlucky52@gmail.com>
2023-03-09 04:20:14 +0000

    nxt trends authentication

---
+++ src/components/Products/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Products = () => (
+  <div className="pro-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+      className="pro-img"
+    />
+  </div>
+)
+
+export default Products
